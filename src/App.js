6763fa4c0048eb5825ef6a5692f612cf7fd650c1
Fahmi Syaifudin <fahmisyaifudin00@gmail.com>
2022-09-28 21:12:24 +0700

    add state increment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+  }
   render() {
     return (
       <div className="App">
@@ -10,8 +19,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button onClick={ this.increment } style={{ marginTop: '20px'}}>Click</button>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This button is clicked {this.state.count} times
         </p>
       </div>
     );
